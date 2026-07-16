709331176a2c47da016538c3237ad2adb996dac0
rlopezes <rlopez.es@gmail.com>
2026-07-16 10:44:29 +0000

    2.	Estructurar los datos

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 url_mad = "https://meteometrics.rlopez.org/historical-data/MAD"
 url_lon = "https://meteometrics.rlopez.org/historical-data/LON"
@@ -8,15 +9,25 @@ try:
     response = requests.get(url_mad)
     response.raise_for_status()
     raw_data_madrid = response.json()
-    print(raw_data_madrid[:2])
-
-    print()
+    
+    # Normalizar y añadir columna para Madrid
+    df_madrid = pd.json_normalize(raw_data_madrid)
+    df_madrid['city'] = 'Madrid'
 
     # Realiza la petición para Londres
     response = requests.get(url_lon)
     response.raise_for_status()
     raw_data_londres = response.json()
-    print(raw_data_londres[:2])
+    
+    # Normalizar y añadir columna para Londres
+    df_londres = pd.json_normalize(raw_data_londres)
+    df_londres['city'] = 'London'
+
+    # Unificar datos en un solo dataframe
+    df_final = pd.concat([df_madrid, df_londres], ignore_index=True)
+    
+    print("Primeras filas del DataFrame unificado:")
+    print(df_final)
 
 except requests.exceptions.RequestException as e:
     print(f'Error en la petición: {e}')
